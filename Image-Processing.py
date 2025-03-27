27684054674063380c4b55cd25632dc74f756f97
Sudipta <powerboy4321@outlook.com>
2025-03-27 22:47:18 +0530

    img_Processing with numpy array

---
+++ Image-Processing.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from google.colab import files
+
+
+uploaded = files.upload()
+
+
+filename = "gans_gfg-(1).jpg"  
+
+
+img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) 
+
+
+if img is None:
+    print("Error: Unable to load image. Check the filename or file format.")
+else:
+   
+    img_arr = np.array(img)
+    
+   
+    print("Shape:", img_arr.shape)
+    print("NumPy Array:\n", img_arr)
